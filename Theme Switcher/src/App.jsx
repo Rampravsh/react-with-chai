7802bcd209da1883e01ef9113750616bbf237ef7
Rampravesh <rampraveshkr4545@gmail.com>
2025-11-23 17:25:06 +0530

    feat: implement theme context and toggle functionality with ThemeBtn and Card components

--- Theme Switcher/src/App.jsx
+++ Theme Switcher/src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
 function App() {
+  const [themeMode, setthemeMode] = useState("light");
+  const lightTheme = () => {
+    setthemeMode("light");
+  };
+  const darkTheme = () => {
+    setthemeMode("dark");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <>
-      <h1>project setup</h1>
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center dark:bg-gray-700 justify-center">
+          <div className="max-w-2xl">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+            <ThemeBtn />
+            <div className="w-full max-w-sm mx-auto"></div>
+            <Card />
+          </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
